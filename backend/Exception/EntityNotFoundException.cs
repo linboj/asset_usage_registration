cafbe592eb5995628c59d06b92fd81de011df29c
linboj <spps2215@gmail.com>
2025-03-12 13:38:03 +0800

    BE-Add Exceptions

--- backend/Exception/EntityNotFoundException.cs
+++ backend/Exception/EntityNotFoundException.cs
@@ -1,8 +1,7 @@
-namespace Backend.Exceptions;
+namespace backend.Exceptions;
 
 public class EntityNotFoundException : Exception
 {
-
     public List<string> Errors { get; set; }
     public EntityNotFoundException(List<string> errors) : base("Entity not found.")
     {
--- backend/Exception/EntityValidationException.cs
+++ backend/Exception/EntityValidationException.cs
@@ -1,8 +1,7 @@
-namespace Backend.Exceptions;
+namespace backend.Exceptions;
 
 public class EntityValidationException : Exception
 {
-
     public List<string> Errors { get; set; }
     public EntityValidationException(List<string> errors) : base("Validation failed.")
     {
